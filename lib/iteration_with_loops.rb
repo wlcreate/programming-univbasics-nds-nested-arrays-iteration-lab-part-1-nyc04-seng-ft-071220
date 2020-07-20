39a740baff0ce6729454c4b09f0c9c37e1533dbc
Waverley Leung <waverley.nyc@gmail.com>
2020-07-20 20:06:59 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,6 +6,7 @@ def find_even_values(src)
   while row_index < src.length do
     element_index = 0
     while element_index < src[row_index].length do
+      
       puts src[row_index][element_index]
       element_index += 1
     end
